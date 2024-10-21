91d2e29512c3729d6c907417aa29f5e5e78c02eb
George Akanza <gakanza@gmail.com>
2024-10-20 20:18:49 -0400

    changes to terraform.yml file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,13 +14,3 @@ provider "aws" {
   # profile = "Fluent-In-DevOps"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "ec2-builder-remote-state"
-    key    = "ec2-image-builder/us-east-2"
-    region = "us-east-2"
-
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "anslem-terraform-tf-state-lock"
-  }
-}
